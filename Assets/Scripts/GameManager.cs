8bdc77b907aa7b8ee63f2009dc3c6a5ca602bfbd
iLikeCars <masonwu60@gmail.com>
2021-09-11 19:40:29 -0700

    Added player movement, camera movement, score text, and game over

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 2f;
+    public void EndGame ()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log(SceneManager.GetActiveScene().name);
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
